c660728e9b65087c2f3edeb75c13a4b355d39543
Kel <me@kelcuprum.ru>
2025-03-24 21:27:20 +0300

    Обновление файлов [2.0.0-alpha.2.1] - fix fix fix fix fix and fix

--- src/main/java/ru/kelcuprum/abi/modules/DefaultModule.java
+++ src/main/java/ru/kelcuprum/abi/modules/DefaultModule.java
@@ -9,15 +9,16 @@ import ru.kelcuprum.alinlib.config.Localization;
 import ru.kelcuprum.alinlib.utils.StealthManager;
 
 public class DefaultModule extends AbstractModule {
+    public static String DEFAULT_MESSAGE = "&6XYZ:&r {player.pos.x} {player.pos.y} {player.pos.z} &6{player.direction_symbol} {world.time_formatted}&r";
     public DefaultModule() {
         super("default", "actionbarinfo", Component.translatable("abi.module.default"));
-        options.add(new Option("INFO", ActionBarInfo.localization.getLocalization("info", false, false, false), Component.translatable("abi.localization.info"), Option.Type.STRING));
+        options.add(new Option("INFO", DEFAULT_MESSAGE, Component.translatable("abi.localization.info"), Option.Type.STRING));
         options.add(new Option("STEALTH", true, Component.translatable("abi.config.stealth"), Option.Type.BOOLEAN));
     }
 
     @Override
     public Component getMessage() {
-        String message = ActionBarInfo.config.getString("INFO", ActionBarInfo.localization.getLocalization("info", false, false, false));
+        String message = ActionBarInfo.config.getString("INFO", DEFAULT_MESSAGE);
         message = Localization.fixFormatCodes(message);
         return Component.literal(AlinLib.localization.getParsedText(message));
     }
